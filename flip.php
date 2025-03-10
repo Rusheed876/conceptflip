3d2d7524891f46892f7c0abc04f29c9cdd5c9901
Whirlwind Content <brownrusheed@gmail.com>
2025-03-10 16:29:01 -0500

    commite

---
+++ flip.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="flip.css">
+    <script src="turn.js"></script>
+</head>
+<body>
+    <div>
+    <img src="https://www.w3schools.com/w3images/fjords.jpg" alt="A beautiful fjord landscape" />
+<img src="https://www.w3schools.com/w3images/rocks.jpg" alt="A scenic view of rocks and water" />
+<img src="https://www.w3schools.com/w3images/forest.jpg" alt="A dense forest during the day" />
+<img src="https://www.w3schools.com/w3images/snow.jpg" alt="Snowy mountains covered with snow" />
+<img src="https://www.w3schools.com/w3images/mountains.jpg" alt="A group of mountains in the distance" />
+<img src="https://www.w3schools.com/w3images/coffee.jpg" alt="A cup of coffee on a table" />
+<img src="https://www.w3schools.com/w3images/lights.jpg" alt="Colorful lights during a celebration" />
+    </div>
+
+
+    <script src="flip.js"></script>
+</body>
+</html>
